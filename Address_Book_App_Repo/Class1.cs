9e95a105db2d552a8765db0a10ff39d3d241b859
Nick Andresen <nimandresen@gmail.com>
2022-06-07 16:19:46 -0400

    Solution and projects added

---
+++ Address_Book_App_Repo/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Address_Book_App_Repo;
+public class Class1
+{
+
+}
---
+++ Address_Book_App_Testing/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Address_Book_App_Testing;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
